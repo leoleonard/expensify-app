fe0081132fa154e2c4bb281e9da7e9df99fc8715
Damian Juzyk <damian.juzyk@gmail.com>
2018-05-09 15:16:51 +0200

    organizing redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-
 const Header = () => (
-    <header>
+  <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true} >dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">add expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active" >help page</NavLink>
-    </header>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
 );
 
 export default Header;
--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const HelpPage = () => (
-    <div>
-    This is a help page
-    </div>
+  <div>
+    This is from my help component
+  </div>
 );
 
 export default HelpPage;
