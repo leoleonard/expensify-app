5cd4748148cdd8a311898f7a0573c09afc937367
Damian Juzyk <damian.juzyk@gmail.com>
2018-05-12 13:22:56 +0200

    add connecting store with redux

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -12,10 +13,20 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
-// store.dispatch(setTextFilter('water'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(()=> {
+    store.dispatch(setTextFilter('rent'));
+}, 3000) 
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
